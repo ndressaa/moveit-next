efec883bba192a7470a3f59db8a24cf7cf82c1ea
Andressa <andressajucoski@gmail.com>
2021-02-25 20:07:43 -0300

    dia 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile () {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/ndressaa.png" alt="Andressa"/>
@@ -8,7 +12,7 @@ export function Profile () {
         <strong>Andressa</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
