c2f93999262b92670232ce70cefb7f553a06f573
Andressa <andressajucoski@gmail.com>
2021-02-25 17:34:08 -0300

    dia 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
+
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
+import { ChallengeBox } from '../components/ChallengeBox'
+
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
@@ -20,7 +23,9 @@ export default function Home() {
           <Countdown />
         </div>
 
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
